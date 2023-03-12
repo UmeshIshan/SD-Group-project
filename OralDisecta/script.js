e00b0c279c230e1f92aea847d68d1d820a287561
Himashi99Kodithuwakku <kodi.99hima@gmail.com>
2023-03-12 23:22:43 +0530

    create uploaded file part

---
+++ OralDisecta/script.js
@@ -0,0 +1,9 @@
+document.getElementById("button").addEventListener("click",function(){
+	
+	document.querySelector(".popup").style.display="flex";
+})
+
+document.querySelector(".closebutton").addEventListener("click",function(){
+	
+	document.querySelector(".popup").style.display="none";
+})
